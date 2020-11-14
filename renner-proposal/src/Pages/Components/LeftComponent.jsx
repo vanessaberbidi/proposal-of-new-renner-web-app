ef4c98700a5ba6c03f9f0bed22aa49105577db77
Vanessa Bernardi Bidinotto <35221680+vanessaberbidi@users.noreply.github.com>
2020-11-14 20:45:51 -0300

    new images

--- renner-proposal/src/Pages/Components/LeftComponent.jsx
+++ renner-proposal/src/Pages/Components/LeftComponent.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import conjunto01 from '../../img/conjunto01.png';
-import conjunto02 from '../../img/conjunto02.png';
-import conjunto03 from '../../img/conjunto03.png';
-import conjunto04 from '../../img/conjunto04.png';
-
+import Images from './Images'
 export default class LeftComponent extends React.Component {
   render() {
     return (
       <div className="content-left">
         <p className="router">HOME - FEMININO - VESTIDOS - VESTIDO FEMININO PRETO COM SAIA RODADA </p>
-        <img className="img-styles" src={conjunto01}/>
-        <img className="img-styles" src={conjunto02}/>
-        <img className="img-styles" src={conjunto03}/>
-        <img className="img-styles" src={conjunto04}/>
+        <Images />
       </div>
     )
   }
