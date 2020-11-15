0db4d3a418dde35deabdbea64e15c6101ad1373e
Lorena Góes <goesloren@gmail.com>
2020-11-15 18:01:20 -0300

    Página de consultoria

--- renner-proposal/src/Pages/StylishConsulting.jsx
+++ renner-proposal/src/Pages/StylishConsulting.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import Header from './Components/Header';
+import Slogan from './Components/Slogan';
+import '../App.css'
 
 export default class StylishConsulting extends React.Component {
   render() {
     return (
-      <h1>Consulting</h1>
+      <div>
+        <Header />
+        <Slogan />
+        <div className="text">
+          Obrigada pelo seu interesse! Nosso time encontrou esses consultores perto de você:
+        </div>
+        <div className="consulting">
+          Ana Silva
+          <a href="#">Entre em contato</a>
+        </div>
+        <div className="consulting">
+          Bruna Oliveira
+          <a href="#">Entre em contato</a>
+        </div>
+        <div className="consulting">
+          José Júnior
+          <a href="#">Entre em contato</a>
+        </div>
+        <div className="consulting">
+          Mário Silva
+          <a href="#">Entre em contato</a>
+        </div>
+        <div className="consulting">
+          Thaís Silveira
+          <a href="#">Entre em contato</a>
+        </div>
+      </div>
     )
   }
 }
