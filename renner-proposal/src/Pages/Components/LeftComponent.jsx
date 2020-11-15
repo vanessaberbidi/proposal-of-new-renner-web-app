b033e67c98922fbc7c6a4e2ece68d1e7c329b778
Lorena Góes <goesloren@gmail.com>
2020-11-14 22:26:22 -0300

    Descrição Roupa

--- renner-proposal/src/Pages/Components/LeftComponent.jsx
+++ renner-proposal/src/Pages/Components/LeftComponent.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Images from './Images'
+import ClothesDetails from '../Components/ClothesDetails'
+
 export default class LeftComponent extends React.Component {
   render() {
     return (
       <div className="content-left">
         <p className="router">HOME - FEMININO - VESTIDOS - VESTIDO FEMININO PRETO COM SAIA RODADA </p>
         <Images />
+        <hr/>
+        <ClothesDetails />
       </div>
     )
   }
