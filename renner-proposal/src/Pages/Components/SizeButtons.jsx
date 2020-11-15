d2c47a06c6f3c4b546797090817ad549468864f3
Vanessa Bernardi Bidinotto <35221680+vanessaberbidi@users.noreply.github.com>
2020-11-14 21:38:13 -0300

    new images

--- renner-proposal/src/Pages/Components/SizeButtons.jsx
+++ renner-proposal/src/Pages/Components/SizeButtons.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 export default class SizeButtons extends React.Component {
   render() {
     return (
-      <div className="size-buttons">
-        <button className="size-button">PP</button>
-        <button className="size-button">P</button>
-        <button className="size-button">M</button>
-        <button className="size-button">G</button>
+      <div className="size-buttons-group">
+        <button className="size-buttons">PP</button>
+        <button className="size-buttons">P</button>
+        <button className="size-buttons">M</button>
+        <button className="size-buttons">G</button>
       </div>
     )
   }
