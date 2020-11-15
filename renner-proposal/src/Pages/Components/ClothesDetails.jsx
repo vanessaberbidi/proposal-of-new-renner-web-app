a4e64f4bbb470cce7ce02d748e0bcf3b5a20241a
Lorena Góes <goesloren@gmail.com>
2020-11-15 15:11:58 -0300

    Descrição para deficientes visuais

--- renner-proposal/src/Pages/Components/ClothesDetails.jsx
+++ renner-proposal/src/Pages/Components/ClothesDetails.jsx
@@ -3,7 +3,9 @@ export default class ClothesDetails extends React.Component {
   render() {
     return (
       <div className="description">
-        <ul className="description-title">DETALHES
+        <br/>
+        <p>DETALHES</p>
+        <ul className="description-title">
           <li>Vestido Feminino Preto</li>
           <li>Liso</li>
           <li>Sem manga e com 1 alça fina</li>
@@ -13,6 +15,15 @@ export default class ClothesDetails extends React.Component {
           <li>Marca: Marfinno</li>
           <li>Material: Viscose</li>
         </ul>
+        <br/>
+        <div>
+          <div>
+            <span>PARA CEGO VER:</span>
+            <p>
+              Quatro imagens com fundo branco, cada uma mostrando duas mulheres, de corpos e etnias diferentes, usando um vestido preto de material viscose da marca Marfinno. O vestido é liso e possui uma alça fina do lado esquerdo, sendo ajustado até a cintura e acompanhando saia rodada na altura do joelho, com pregas grandes na saia.
+            </p>
+          </div>
+        </div>
       </div>
     )
   }
